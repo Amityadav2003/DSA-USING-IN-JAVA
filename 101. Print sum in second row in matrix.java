54059c79126a6e6a320f7d404c33549fb0af0508
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-11 12:31:53 +0530

    Update and rename 03. take array name input.java to 101. Print sum in second row in matrix.java

---
+++ 101. Print sum in second row in matrix.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class JavaBasics{
+
+public static void main(String arg[]){
+int matrix[][]={{1,2,3,7},
+                {4,5,6,7},
+                {7,8,9,10}};
+int sum=0;
+for(int i=0; i<matrix[0].length;i++){
+  sum+=matrix[1][i];
+}
+System.out.println("Sum is :"+sum);
+}
+}
+
+
+
+
